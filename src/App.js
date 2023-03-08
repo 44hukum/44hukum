fa0f58a15e9bec4fd86c8267e769c996d95a73db
44hukum <hukum.gosain@resimator.fi>
2023-03-08 23:14:28 +0545

    Initial threejs setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
+function Cylinder3d(props){
+  const ref = useRef();
+  const [hovered, hover]=useState(false)
+  const [clicked, click] = useState(false)
+
+  useFrame((state, delta)=> (ref.current.rotation.x += 0.01))
+
+  return (
+    <mesh {...props} ref={ref} scale={clicked ? 1.5:1}
+    onClick={(event) => hover(true)}
+    onPointerOver={(event) => hover(true)}
+    onPointerOut={(event) => hover(false)}
+    > 
+      <cylinderGeometry args={[1,1,1]} />
+      <meshStandardMaterial wireframe={props.wireframe} color={hovered ? "hotpink": "orange"} />
+    </mesh>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Canvas>
+      <ambientLight />
+        	<pointLight position={[10, 10, 10]} />
+        <Cylinder3d position={[-1.2, 0, 0]}/>
+        <Cylinder3d position={[1.2, 0, 0]}/>
+        <Cylinder3d position={[1.4, 0, 0]}/>
+        <Cylinder3d position={[-1.4, 0, 0]}/>
+
+
+        <Cylinder3d position={[-2.2, 0, 0]}/>
+        <Cylinder3d position={[2.2, 0, 0]}/>
+        <Cylinder3d position={[2.4, 0, 0]}/>
+        <Cylinder3d position={[-2.4, 0, 0]}/>
+      </Canvas>
+
+      <h1 style={{'color':'white'}}>
+      Hello world
+
+      </h1>
+    </>
+    );
 }
 
 export default App;
